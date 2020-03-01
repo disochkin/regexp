ec95d07e510a08e632e8a221752f8991bbc20e00
Denis Nagovitsyn <disochkin@gmail.com>
2020-03-01 23:50:32 +0500

    исправил замечания

--- main.py
+++ main.py
@@ -1,39 +1,38 @@
 import re
 # "слияние" списков
+
+
 def merge_lists(list1, list2):
-    result = ["", "", "", "", "", "", ""]
+    result = []
     for k in range(len(list2)):
         if list2[k] != "":
-            result[k] = list2[k]
+            result.append(list2[k])
         else:
-            result[k] = list1[k]
+            result.append(list1[k])
     return result
+
+
+def get_empty_list(count):
+    result = []
+    for item in range(count):
+        result.append("")
+    return result
+
 
 # исправление формата телефонного номера к виду +7(999)999-99-99 доб.9999;
 def fix_number_format(number):
-    number = re.sub(r"^[\7|8]", "+7", number)
-    digit = []
-    additional_digit = " доб."
-    for j in re.finditer(r"\d", number):
-        digit.append(j.group())
-    result = (
-        f'+{digit[0]}({digit[1]}{digit[2]}{digit[3]}){digit[4]}{digit[5]}{digit[5]}-{digit[6]}{digit[6]}-{digit[8]}{digit[9]}')
-    for j in range(len(digit)):
-        if j > 10:
-            additional_digit = additional_digit + f'{digit[j]}'
-    if len(additional_digit) > 5:
-        result = result + additional_digit
+    digits = re.sub(r'\D', "", number)
+    result = "+{}({}{}{}){}{}{}-{}{}-{}{}".format(*digits)
+    if digits[11:]:
+        result = result + " доб." + digits[11:]
     return result
 
-
-from pprint import pprint
 # читаем адресную книгу в формате CSV в список contacts_list
 import csv
 
 with open("phonebook_raw.csv") as f:
     rows = csv.reader(f, delimiter=",")
     contacts_list = list(rows)
-#pprint(contacts_list)
 
 # TODO 1: выполните пункты 1-3 ДЗ
 # упорядочивание расположения ФИО по колонкам
@@ -47,26 +46,25 @@ for item_contact_list in contacts_list:
 extended_dict = {}
 i = 0
 for i in range(len(contacts_list)):
-    extended_dict[contacts_list[i][0] + " " + contacts_list[i][1]] = \
-        merge_lists(extended_dict.get(contacts_list[i][0] + " " + contacts_list[i][1], ["", "", "", "", "", "", ""]),
-                    contacts_list[i])
+    key_name = contacts_list[i][0] + " " + contacts_list[i][1]
+    extended_dict[key_name] = merge_lists(extended_dict.get(key_name, get_empty_list(len(contacts_list[0]))), contacts_list[i])
+
+#словарь приводим к списку
+contacts_list.clear()
+for key in extended_dict.keys():
+    contacts_list.append(extended_dict[key])
 
 # приводим телефонный номер к формату +7(999)999-99-99 доб.9999;
-for i in range(len(extended_dict.values())):
+for i in range(len(contacts_list)):
     if i > 1:
-        #print(extended_dict.values)
+        print(contacts_list[i][5])
+        print(fix_number_format(contacts_list[i][5]))
+        contacts_list[i][5] = fix_number_format(contacts_list[i][5])
 
-        extended_dict.values()[i][5] = fix_number_format(extended_dict.values()[i][5])
-    #print(item[6])
-
-
-
-# исправление формата телефонного номера
-
-
+print(contacts_list)
 # TODO 2: сохраните получившиеся данные в другой файл
 # код для записи файла в формате CSV
-# with open("phonebook.csv", "w") as f:
-#     datawriter = csv.writer(f, delimiter=',')
-#     # Вместо contacts_list подставьте свой список
-#     datawriter.writerows(contacts_list)
+with open("phonebook.csv", "w") as f:
+     datawriter = csv.writer(f, delimiter=',')
+     # Вместо contacts_list подставьте свой список
+     datawriter.writerows(contacts_list)
--- test.py
+++ test.py
@@ -1,32 +1,10 @@
-# def merge_lists(list1,list2):
-#     result = list()
-#     for i in range(len(list2)):
-#         if list2[i] != "":
-#             result.append(list2[i])
-#         else:
-#             result.append(list1[i])
-#     return result
-#
-#
-# list1 = ['Мартиняхин', 'Виталий', 'Геннадьевич', 'ФНС', '', '+74959130037', '']
-# list2 = ['Мартиняхин', 'Виталий', 'Геннадьевич', 'ФНС', 'cоветник отдела Интернет проектов Управления информационных технологий', '', '']
 import re
-tel = ["+7 (495) 913-04-78", "+74959130037","8 495-913-0168","+7 (495) 983-36-99 доб. 2926","8(495)748-49-73","+7 (495) 913-11-11 (доб. 0792)"]
+# result = re.match(r'\d', '+7 (495) 983-36-99 доб. 2926')
+# print (result)
 
-for i in range(len(tel)):
-    tel[i] = re.sub(r"^[\7|8]", "+7", tel[i])
-    digit =[]
-    additional_digit = " доб."
-    for j in re.finditer(r"\d", tel[i]):
-        digit.append(j.group())
-    result = (f'+{digit[0]}({digit[1]}{digit[2]}{digit[3]}){digit[4]}{digit[5]}{digit[5]}-{digit[6]}{digit[6]}-{digit[8]}{digit[9]}')
-    for j in range(len(digit)):
-        if j > 10:
-            additional_digit = additional_digit + f'{digit[j]}'
+born = "+7 (495) 983-36-99 доб. 2926"
 
-    if len(additional_digit) > 5:
-        result = result + additional_digit
-
-    print(result)
-
-
+# Удалим комментарий из строки
+dob = re.sub(r'\D', "", born)
+print("Дата рождения:", dob)
+print("Дата рождения:", dob[11:])
